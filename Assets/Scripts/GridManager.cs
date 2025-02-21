3021596af0ec6941a3e20c1f379eca91a12056de
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-21 18:17:43 +0700

    Initial Setup

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+    [SerializeField] private Tile tilePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

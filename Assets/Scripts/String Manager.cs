6f2a88114cff36914e1b5056a39a5ba9ffd081c3
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-02-25 13:24:45 +0700

    Main Menu

---
+++ Assets/Scripts/String Manager.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+public class StringManager : MonoBehaviour
+{
+    // ===========================
+    // Scene Manager
+    // ===========================
+
+    public static String tutorialScene = "Tutorial Scene";
+    public static String homeScene = "Main Menu";
+}
